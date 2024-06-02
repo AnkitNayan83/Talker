7c4ade2a7cd348e174b98764aab1c965e8933aed
AnkitNayan83 <ankitnayan83@gmail.com>
2024-06-02 11:54:31 +0530

    commit

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -16,11 +16,11 @@ router.get("/feed", getFeedPost);
 router.get("/:id", userAuth, getPost);
 
 router.post("/", userAuth, createPost);
-router.post("/unlike/:id", userAuth, unlikePost);
 router.post("/:id", userAuth, likePost);
 
 router.put("/:id", userAuth, updatePost);
 
+router.delete("/unlike/:id", userAuth, unlikePost);
 router.delete("/:id", userAuth, deletePost);
 
 export default router;
