5dcde4c0ed6a61e69d75ed5178d4b58000ee5cd4
AnkitNayan83 <ankitnayan83@gmail.com>
2024-06-08 10:50:42 +0530

    commit

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post("/register", body("email").isEmail().normalizeEmail(), Register);
 router.post("/login", body("email").isEmail().normalizeEmail(), Login);
+
 router.post("/verify-email", VerifyEmail);
 router.post("/verify-jwt", userAuth, verifyJWTToken);
 
